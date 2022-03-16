2f0d6f9e73611eb748c1d44fbadafd97c740462a
VLADIMIR BURLYAEV <eliseevmir@gmail.com>
2022-03-16 17:56:59 +0500

    feat: popup add new card and new cards per page

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,88 @@
-const createBtn = document.querySelector(".profile__button-create");
-const closePopupBtn = document.querySelector(".popup__close");
-const popup = document.querySelector(".popup");
-const popupForm = document.querySelector(".popup__form");
+const createProfileBtn = document.querySelector(".profile__button-create");
+const addCardsBtn = document.querySelector(".profile__button-add");
+
+const closePopupProfileBtn = document.querySelector(".popup__close-profile");
+const closePopupCardsBtn = document.querySelector(".popup__close-cards");
+
+const popupFormProfile = document.querySelector(".popup__form-profile");
+const popupFormCards = document.querySelector(".popup__form-cards");
+
 const formName = document.querySelector(".profile__title");
 const formInfo = document.querySelector(".profile__subtitle");
-const popupBtn = document.querySelector(".popup__button");
+
+const popupCreateProfile = document.querySelector(".popup_create-profile");
+const popupCardsAdd = document.querySelector(".popup_cards-add");
 
 let popupInputName = document.querySelector("input[name ='user-name']");
 let popupInputInfo = document.querySelector("input[name ='user-info']");
 
-function openPopup () {
+function openPopup (arg) {
+  arg.classList.add("popup_opened")
+}
+
+function closePopup (arg) {
+  arg.classList.remove("popup_opened")
+}
+
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  formName.textContent = popupInputName.value;
+  formInfo.textContent = popupInputInfo.value;
+  closePopup(popupCreateProfile)
+};
+
+createProfileBtn.addEventListener("click", ()=> {
   popupInputName.setAttribute("value", formName.textContent);
   popupInputInfo.setAttribute("value", formInfo.textContent);
-  popup.classList.add("popup_opened");
-};
+  openPopup(popupCreateProfile)
+})
 
-function closePopup () {
-  popup.classList.remove("popup_opened");
-};
+closePopupProfileBtn.addEventListener("click", ()=>{
+  closePopup(popupCreateProfile)
+})
 
-function handleProfileFormSubmit (event) {
-    event.preventDefault();
-    formName.textContent = popupInputName.value;
-    formInfo.textContent = popupInputInfo.value;
-    closePopup()
-};
+popupFormProfile.addEventListener("submit", handleProfileFormSubmit);
 
-createBtn.addEventListener("click", openPopup);
-closePopupBtn.addEventListener("click", closePopup);
-popupForm.addEventListener("submit", handleProfileFormSubmit);
+addCardsBtn.addEventListener("click", ()=> {
+  openPopup(popupCardsAdd)
+})
+
+closePopupCardsBtn.addEventListener("click", ()=> {
+  closePopup(popupCardsAdd)
+})
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (item) {
+  const templaneCards = document.querySelector(".template__cards").content;
+  const cardsItem = templaneCards.querySelector(".cards__item").cloneNode(true);
+  cardsItem.querySelector(".cards__image").src = item.link;
+  cardsItem.querySelector(".cards__image").alt = item.name;
+  cardsItem.querySelector(".cards__text").textContent = item.name;
+  document.querySelector(".cards").append(cardsItem);
+})
